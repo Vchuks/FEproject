6239d12298094a319e06e540fdb6e22231557737
Vchuks <64593598+Vchuks@users.noreply.github.com>
2025-02-11 11:54:41 +0100

    project setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <h2 className='text-center'>Wish you the best!</h2>
+      <p className='text-center'>Make sure to create your branch and follow the instructions in readme.md</p>
     </div>
   );
 }
